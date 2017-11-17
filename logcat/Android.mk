3b1c2fc70ba522db7416ec420ccda5cb5ed46922
Lukas0610 <mail@lukasberger.at>
2017-11-17 17:21:52 +0100

    logcat: properly set up logcatd-service

--- logcat/Android.mk
+++ logcat/Android.mk
@@ -20,6 +20,7 @@ LOCAL_MODULE_TAGS := debug
 LOCAL_SRC_FILES := logcatd_main.cpp event.logtags
 LOCAL_SHARED_LIBRARIES := liblogcat $(logcatLibs)
 LOCAL_CFLAGS := -Werror
+LOCAL_INIT_RC := logcatd.rc
 
 include $(BUILD_EXECUTABLE)
 
@@ -39,7 +40,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := logpersist.start
 LOCAL_MODULE_TAGS := debug
 LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_INIT_RC := logcatd.rc
 LOCAL_MODULE_PATH := $(bin_dir)
 LOCAL_SRC_FILES := logpersist
 ALL_TOOLS := logpersist.start logpersist.stop logpersist.cat
